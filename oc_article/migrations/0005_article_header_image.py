90a7248c80d10ac3201f5f594712399638b0242a
Sævar Öfjörð Magnússon <saevar@overcast.io>
2015-06-30 15:19:32 +0000

    Missing migration

---
+++ oc_article/migrations/0005_article_header_image.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wagtailimages', '0006_add_verbose_names'),
+        ('oc_article', '0004_auto_20150626_1227'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='article',
+            name='header_image',
+            field=models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='wagtailimages.Image', null=True),
+        ),
+    ]
